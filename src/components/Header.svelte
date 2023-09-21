3681b7b21c80eafff7f3a7c60370dcc426031732
Kevin <kwosborne.pt@gmail.com>
2023-09-21 19:14:19 +0100

    Init
    Landing Page
    Routing

---
+++ src/components/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<div>
+    <nav class=" w-full flex justify-between pr-36 pl-36 pt-10 text-xl text-white font-bold">
+        <div>
+            <a href="/">
+                <img class="w-10 h-10 rounded-2xl object-cover justify-self-start" src="/favicon.svg" alt="logo" />
+            </a>
+        </div>
+        <div class="flex gap-x-12 ">
+            <a class="hover:text-gray-200" href="/">
+                Home
+            </a>
+            <a class="hover:text-gray-200" href="/about">
+                About
+            </a>
+            <a class="hover:text-gray-200" href="/contact">
+                Contact
+            </a>
+        </div>
+    </nav>
+</div>
+
+<style>
+
+</style>
---
+++ src/components/LandingHero.svelte
@@ -0,0 +1,12 @@
+<div class="p-8">
+    <div class="grid grid-cols-2">
+        <div class="flex flex-col gap-8 pr-32 pl-16 py-48">
+            <small class="text-sm font-bold text-purple-200 uppercase">our mission</small>
+            <h1 class=" text-4xl font-bold text-white">To make music better every day.</h1>
+            <p class=" text-xl text-white">You can rest assured that your life's work is safe within our hands.</p>
+        </div>
+        <div style="background-image:url('/heroBackground.png')" class="bg-no-repeat bg-cover bg-center overflow-hidden max-h-[95%] mt-5">
+
+        </div>
+    </div>
+</div>
